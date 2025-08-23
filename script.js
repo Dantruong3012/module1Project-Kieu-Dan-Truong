dcf5ec688897a74d9fc640eb574298e328025d2d
Dan Truong <dantruong@MacBook-Air-cua-Dan.local>
2025-08-23 10:09:05 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,118 @@
+class Product {
+  constructor(name, price, img, quantity) {
+    this.name = name;
+    this.price = price;
+    this.img = img;
+    this.quantity = quantity;
+  }
+}
+
+class Cart {
+  constructor() {
+    this.items = [];
+  }
+  addToCart(product) {
+    this.items.push(product);
+    this.displayCart();
+  }
+
+  removeFromCart(index) {
+    this.items.splice(index, 1);
+    this.displayCart();
+  }
+
+  updateQuantity(index, newQuantity) {
+    this.items[index].quantity = parseInt(newQuantity);
+    this.displayCart();
+  }
+paying(index){
+    const item = this.items[index];
+    const confirmPaying = confirm(`Are you sure, you want to pay for this item ${item.name} !`)
+    if(confirmPaying){
+        alert("You have paid successfully!");
+        this.items.splice(index, 1)
+        this.displayCart();
+    }
+}
+  displayCart() {
+    const showCart = document.getElementById("cart-body");
+    let total = 0;
+ let cartBody = `<table class="cart-body" border="1" cellspacing="0" cellpadding="5">
+     <thead>
+        <tr>
+          <th>Product Name</th>
+          <th>Picture</th>
+          <th>Price</th>
+          <th>Quantity</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+      `;
+      this.items.forEach((item, index) => {
+    total += item.price * item.quantity;
+    cartBody += `
+      <tr>
+        <td>${item.name}</td>
+        <td><img src="${item.img}" width="60"/></td>
+        <td>${item.price.toLocaleString()}</td>
+        <td>${item.quantity}</td>
+        <td><button onclick="cart.removeFromCart(${index})">
+        <i class="fa-solid fa-trash"></i> Delete</button>
+       <button class="pay-btn" onclick="cart.paying(${index})">
+    <i class="fa-solid fa-credit-card"></i> Pay
+  </button>
+      </tr>
+    `;
+  });
+  cartBody += `
+      </tbody>
+      <tfoot>
+        <tr>
+          <td colspan="4" style="text-align:right;"><strong>Total:</strong></td>
+          <td>${total.toLocaleString()}</td>
+        </tr>
+      </tfoot>
+    </table>
+  `;
+  showCart.innerHTML = cartBody;
+  }
+}
+const cart = new Cart();
+function add() {
+  event.preventDefault();
+  const name = document.getElementById("productName").value;
+  const price = document.getElementById("inputPrice").value;
+  const img = document.getElementById("inputPicture").value;
+  const quantity = document.getElementById("inputQuantity").value;
+
+  if (!name || !price || !quantity) {
+    alert("Make sure you enter all of the necessary information!");
+  }
+
+  const product = new Product(name, price, img, quantity);
+  cart.addToCart(product);
+}
+const productDemo1 = new Product(
+  "Blue t-shirt",
+  500000,
+  "pictures/blueT-Shirt.jpeg",
+  1
+);
+
+const productDemo2 = new Product(
+  "Iphone 16 Pro Max",
+  27000000,
+  "pictures/iphone.jpeg",
+  1
+);
+const productDemo3 = new Product(
+  "Adidas Sneaker",
+  350000,
+  "pictures/sneaker.jpeg",
+  2
+);
+
+cart.addToCart(productDemo1);
+cart.addToCart(productDemo2);
+cart.addToCart(productDemo3);
